6ae7231c711c3a51c531717eadc3bb426660c24b
Thao Ha <thao.ha@asnet.com.vn>
2025-09-03 14:22:00 +0700

    feat: remove redundant code

--- price-gold/src/App.tsx
+++ price-gold/src/App.tsx
@@ -1,10 +1,11 @@
-import './App.css';
+import { Button } from './components/common/Button';
 
 function App() {
   return (
     <>
       <div>
-        <h1>Hello world</h1>
+        <h1>Hello world1</h1>
+        <Button>Click me</Button>
       </div>
     </>
   );
