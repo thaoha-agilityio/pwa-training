20a9001ef4f165e77ce7dd2646c23bd1da45b9d9
Thao Ha <thao.ha@asnet.com.vn>
2025-09-03 14:21:27 +0700

    feat: config shadcn/ui

--- price-gold/vite.config.ts
+++ price-gold/vite.config.ts
@@ -1,11 +1,14 @@
 import { VitePWA } from 'vite-plugin-pwa';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindcss(),
     VitePWA({
       registerType: 'prompt',
       injectRegister: false,
@@ -36,4 +39,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 });
