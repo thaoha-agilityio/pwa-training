ba440648d70172966c902c251ff237d0969da473
Thao Ha <thao.ha@asnet.com.vn>
2025-09-05 14:59:23 +0700

    feat: implement daily change

---
+++ price-gold/src/constants/gold.ts
@@ -0,0 +1,22 @@
+import { GoldPrice } from '@/types';
+
+export const INIT_GOLD_PRICE: GoldPrice = {
+  date: '',
+  price: 0,
+  rates: {
+    XAU: 0,
+  },
+};
+
+export const INIT_GOLD_PRICE_HISTORICAL = {
+  start_rate: 0,
+  end_rate: 0,
+  rates: {
+    XAU: {
+      change: 0,
+      change_pct: 0,
+      end_rate: 0,
+      start_rate: 0,
+    },
+  },
+};
--- price-gold/src/pages/Home/index.tsx
+++ price-gold/src/pages/Home/index.tsx
@@ -13,18 +13,31 @@ import {
 import { CURRENCIES_OPTIONS } from '@/constants';
 
 // Hooks
-import { useDebouncedCallback, useLatestPriceGold } from '@/hooks';
+import {
+  useDebouncedCallback,
+  useHistoricalPriceGold,
+  useLatestPriceGold,
+} from '@/hooks';
 
 export const Home = () => {
   const [currency, setCurrency] = useState(CURRENCIES_OPTIONS[0].value);
 
-  const { data, isFetching, refetch } = useLatestPriceGold(currency);
+  // Queries
+  const {
+    data: latestData,
+    isFetching,
+    refetch,
+  } = useLatestPriceGold(currency);
 
-  const currencyRate = data.rates;
+  const { data: historicalData } = useHistoricalPriceGold(currency);
 
+  // Latest rates
+  const { rates: latestRates } = latestData || {};
+  const { rates: historicalRates } = historicalData || {};
+
+  // Helpers
   const isUSD = currency === CURRENCIES_OPTIONS[0].value;
-
-  const latestPrice = isUSD ? currencyRate.USDXAU : currencyRate.EURXAU;
+  const latestPrice = isUSD ? latestRates.USDXAU : latestRates.EURXAU;
 
   const TABS_DATA = [
     {
@@ -32,18 +45,18 @@ export const Home = () => {
       label: 'Gold',
       content: (
         <PriceGold
-          latestPrice={latestPrice}
-          change={0}
-          changePercent={0}
-          currency={currency}
           isUSD={isUSD}
+          latestPrice={latestPrice}
+          change={historicalRates.XAU.change}
+          changePercent={historicalRates.XAU.change_pct}
+          currency={currency}
         />
       ),
     },
     {
       value: 'sliver',
       label: 'Sliver',
-      content: <Typography>€3,097.30 EUR</Typography>,
+      content: <Typography>Sliver</Typography>,
     },
     {
       value: 'platinum',
